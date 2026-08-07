b03bb43a97a582e187a19a0c6b73d443166ca3b6
oh-seungjun <sunjung272@gmail.com>
2026-08-07 15:40:09 +0900

    Create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) # 짧은 글자 데이터
+    content = models.TextField() # 긴 글
+    created_at = models.DateTimeField(auto_now_add=True) # 글 작성 시간 자동 저장
+
+    def __str__(self):
+        return self.title # 작성한 title로 표시하게 해줌
