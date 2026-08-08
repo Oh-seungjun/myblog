a211d88b8e230d42bdc29432d5ff5b30541cd4b8
oh-seungjun <sunjung272@gmail.com>
2026-08-08 18:07:20 +0900

    Add post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,8 @@ def home(request):
     posts = Post.objects.all()
 
     return render(request, "blog/home.html", {"posts": posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    return render(request, "blog/post_detail.html", {"post": post})
